fa9a5ec11ca51b2a34babf11e857e84030bfdd4c
MaxDev <maks.ilyakhin.007@gmail.com>
2025-08-09 20:00:32 +0400

    Обновление ссылок + форматирование

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,34 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 const phasermsg = () => {
     return {
         name: 'phasermsg',
         buildStart() {
-            process.stdout.write(`Building for production...\n`);
+            process.stdout.write(`Building for production...\n`)
         },
         buildEnd() {
-            const line = "---------------------------------------------------------";
-            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-            process.stdout.write(`${line}\n${msg}\n${line}\n`);
+            const line = '---------------------------------------------------------'
+            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`
+            process.stdout.write(`${line}\n${msg}\n${line}\n`)
 
-            process.stdout.write(`✨ Done ✨\n`);
+            process.stdout.write(`✨ Done ✨\n`)
         }
     }
 }
 
 export default defineConfig({
     base: './',
-    plugins: [
-        vue(),
-        phasermsg()
-    ],
+    plugins: [vue(), phasermsg()],
     resolve: {
         alias: {
-          '@': fileURLToPath(new URL('../src', import.meta.url))
+            '@': fileURLToPath(new URL('../src', import.meta.url))
         }
-      },
+    },
     logLevel: 'warning',
     build: {
         rollupOptions: {
@@ -49,4 +46,4 @@ export default defineConfig({
             }
         }
     }
-});
+})
