aa3aae74c7062fccad5da06b643ad0f3b7e9da4f
MaxDev <maks.ilyakhin.007@gmail.com>
2025-08-09 19:34:44 +0400

    Форматирование проекта

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,9 +1,9 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import { Boot } from './scenes/Boot'
+import { GameOver } from './scenes/GameOver'
+import { Game as MainGame } from './scenes/Game'
+import { MainMenu } from './scenes/MainMenu'
+import { AUTO, Game } from 'phaser'
+import { Preloader } from './scenes/Preloader'
 
 // Find out more information about the Game Config at:
 // https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
@@ -13,19 +13,11 @@ const config: Phaser.Types.Core.GameConfig = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
-};
-
-const StartGame = (parent: string) => {
-
-    return new Game({ ...config, parent });
-
+    scene: [Boot, Preloader, MainMenu, MainGame, GameOver]
 }
 
-export default StartGame;
+const StartGame = (parent: string) => {
+    return new Game({ ...config, parent })
+}
+
+export default StartGame
