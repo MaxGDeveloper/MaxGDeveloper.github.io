831d55cc44f4389807bbb1a02946c8a70fa41872
MaxDev <maks.ilyakhin.007@gmail.com>
2025-08-17 23:58:03 +0400

    Поменял местами событие анимации и запуска новой сцены после нее

--- src/utils/sceneTransition.ts
+++ src/utils/sceneTransition.ts
@@ -1,9 +1,9 @@
 export function transitionTo(currentScene: Phaser.Scene, targetKey: string, duration = 400) {
     const camera = currentScene.cameras.main
 
+    camera.fadeOut(duration, 0, 0, 0)
+
     camera.once('camerafadeoutcomplete', () => {
         currentScene.scene.start(targetKey)
     })
-
-    camera.fadeOut(duration, 0, 0, 0)
 }
